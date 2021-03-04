aba98c126de271e38478699d44f54eb9febb7dbe
Farooq Alwi <farooqalwi313@gmail.com>
2021-03-04 05:49:28 +0500

    working to make json file

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 from telethon.sync import TelegramClient
+import json
 
-api_id=1234567
-api_hash='faroq'
+api_id=3172543
+api_hash='f7f77128624651fb67'
 
 client = TelegramClient('test_session', api_id, api_hash)
 client.start()
 
-for message in client.iter_messages('vuopak', limit=2, reverse=True):
+# to store json data in a list
+json_list = list()
+
+
+
+for message in client.iter_messages('vuopak', limit=1, reverse=True):
     # print(dir(message))
-    print(message.date)
-    print(message.id)
+    # print(message.date)
+    # print(message.id)
     # print(message.text)
     # print(message.raw_text)
-    # print(message.to_json())
+    json_list.append(message.to_json())
     # client.download_media(message)
+
+
+print(json_list)
+
+with open('result.json', 'w', encoding='utf-8') as file:
+    json.dump(json_list, file, ensure_ascii=False, indent=1)
