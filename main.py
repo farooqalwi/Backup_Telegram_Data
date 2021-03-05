d87a513ef869bba41eb7fb366ff20a26bd707c61
Farooq Alwi <farooqalwi313@gmail.com>
2021-03-05 06:03:27 +0500

    getting data from telegram in string format and trying to change it into dictionary

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from telethon.sync import TelegramClient
 import json
+import ast    # to convert strign to dic
 
-api_id=3172543
-api_hash='f7f77128624651fb67'
+api_id=3175343146456
+api_hash='39da8f7f70b84e0a71'
 
 client = TelegramClient('test_session', api_id, api_hash)
 client.start()
 
 # to store json data in a list
-json_list = list()
-
+json_dict = dict()
 
 
 for message in client.iter_messages('vuopak', limit=1, reverse=True):
@@ -18,11 +18,17 @@ for message in client.iter_messages('vuopak', limit=1, reverse=True):
     # print(message.id)
     # print(message.text)
     # print(message.raw_text)
-    json_list.append(message.to_json())
+    
+    # string_to_dic = ast.literal_eval(message.to_json())
+    # print(type(message.to_json()))
+    json_dict = message.to_json()
+
     # client.download_media(message)
 
+# import jsonpickle
+# json_dict = jsonpickle.encode(json_dict)
+# print(type(json_dict))
+# print(json_dict)
 
-print(json_list)
-
-with open('result.json', 'w', encoding='utf-8') as file:
-    json.dump(json_list, file, ensure_ascii=False, indent=1)
+# with open('result.json', 'w', encoding='utf-8') as file:
+#     json.dump(json_list, file, ensure_ascii=False, indent=1)
