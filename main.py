37b007aeb79368eff5a4c5098bd0e0f871f87277
Farooq Alwi <farooqalwi313@gmail.com>
2021-03-06 06:01:06 +0500

    work done with one object

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 from telethon.sync import TelegramClient
 import json
 
-api_id=31753431
-api_hash='39da8f7f70b84e0a7128624651fb6706'
+api_id=317
+api_hash='39da8f7f70b8'
 
 client = TelegramClient('test_session', api_id, api_hash)
 client.start()
 
 # to store json data in a dic
 json_dict = dict()
+# json_list = list()
 
-def addItem(key, value):
-    json_dict[key] = value
+# def addItem(key, value):
+#     json_dict[key] = value
 
 for message in client.iter_messages('vuopak', limit=1, reverse=True):
     # print(dir(message))
@@ -19,13 +20,18 @@ for message in client.iter_messages('vuopak', limit=1, reverse=True):
     # print(message.id)
     # print(message.text)
     # print(message.raw_text)
+    # print(message.to_json())
+
+
     json_string = message.to_json()
 
     # convert string to dictionary
     str_dict = json.loads(json_string) 
     for key, value in str_dict.items():
         if key == "id" or key == "date" or key == "photo" or key == "file" or key == "thumbnail":
-            addItem(key, value)
+            json_dict[key] = value
+        
+    # json_list.append(json_dict)
 
     # client.download_media(message)
 
