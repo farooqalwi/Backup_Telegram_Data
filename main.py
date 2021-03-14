d742781da9fdc30cf3679977a06a4f835c993ad3
Farooq Alwi <farooqalwi313@gmail.com>
2021-03-14 19:57:52 +0500

    working for file attributes

--- main.py
+++ main.py
@@ -25,22 +25,47 @@ for message in client.iter_messages('vuopak', limit=10, reverse=True):
     # print(message.get_entities_text())
     # print(client.download_media(message))
 
-    if message.photo is not None:
-        print("photo id: ", message.photo.id)
-        print("photo date: ", message.photo.date)
-        print("photo dc_id: ", message.photo.dc_id)
-        print("photo access_hash: ", message.photo.access_hash)
-        print("photo file_reference: ", message.photo.file_reference)
-        # print("photo from_reader: ", message.photo.from_reader())
-        print("photo has_stickers: ", message.photo.has_stickers)
-        # print("photo pretty_format: ", message.photo.pretty_format())
-        # print("photo iserialize_bytes: ", message.photo.serialize_bytes())
-        # print("photo serialize_datetime: ", message.photo.serialize_datetime())
-        print("photo sizes: ", message.photo.sizes())
-        print("photo stringify: ", message.photo.stringify())
-        print("photo video_sizes: ", message.photo.video_sizes)
-        print("photo to_dict: ", message.photo.to_dict())
-        print("photo to_json: ", message.photo.to_json())
+    
+    # # photo attributes
+    # print(dir(message.photo))
+
+    # if message.photo is not None:
+    #     print("photo id: ", message.photo.id)
+    #     print("photo date: ", message.photo.date)
+    #     print("photo dc_id: ", message.photo.dc_id)
+    #     print("photo access_hash: ", message.photo.access_hash)
+    #     print("photo file_reference: ", message.photo.file_reference)
+    #     # print("photo from_reader: ", message.photo.from_reader())
+    #     print("photo has_stickers: ", message.photo.has_stickers)
+    #     # print("photo pretty_format: ", message.photo.pretty_format())
+    #     # print("photo iserialize_bytes: ", message.photo.serialize_bytes())
+    #     # print("photo serialize_datetime: ", message.photo.serialize_datetime())
+    #     print("photo sizes: ", message.photo.sizes())
+    #     print("photo stringify: ", message.photo.stringify())
+    #     print("photo video_sizes: ", message.photo.video_sizes)
+    #     print("photo to_dict: ", message.photo.to_dict())
+    #     print("photo to_json: ", message.photo.to_json())
+    # else:
+    #     print("None")
+
+
+    # file attributes
+    # print(dir(message.file))
+
+    if message.file is not None:
+        print("file id: ", message.file.id)
+        print("file name: ", message.file.name)
+        print("file title: ", message.file.title)
+        print("file duration: ", message.file.duration)
+        print("file emoji: ", message.file.emoji)
+        print("file ext: ", message.file.ext)
+        print("file width: ", message.file.width)
+        print("file height: ", message.file.height)
+        print("file media: ", message.file.media)
+        print("file mime_type: ", message.file.mime_type)
+        print("file performer: ", message.file.performer)
+        print("file size: ", message.file.size)
+        print("file sticker_set: ", message.file.sticker_set)
     else:
         print("None")
 
